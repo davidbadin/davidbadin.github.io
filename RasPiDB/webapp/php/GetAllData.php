ff04b48c5ba34732cbabd30996dea44e1d7900c6
davidbadin <diegofilm24@gmail.com>
2020-07-30 13:15:05 +0000

    a

--- RasPiDB/webapp/php/GetAllData.php
+++ RasPiDB/webapp/php/GetAllData.php
@@ -12,16 +12,16 @@ if ($conn->connect_error) {
 	die("Connection failed: " . $conn->connect_error);
 }
 
- $sql = "select * from 'TestTable'";
+$sql = "select * from TestTable";
  
- $res = mysqli_query($conn,$sql);
+$res = mysqli_query($conn,$sql);
  
- $result = array();
+$result = array();
  
- while($row = mysqli_fetch_array($res)){
- array_push($result, 
- array('Id'=>$row[0],'Name'=>$row[1],'Rating'=>$row[2],'Date'=>$row[3]));
- }
+while($row = mysqli_fetch_array($res)){
+	array_push($result, 
+	array('Id'=>$row[0],'Name'=>$row[1],'Rating'=>$row[2],'Date'=>$row[3]));
+}
  
  echo json_encode(array('result'=>$result));
 ?>
