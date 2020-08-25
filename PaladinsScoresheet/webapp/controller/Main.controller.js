646ddac851e7ad4266082c9a3de331e189f98ae9
davidbadin <61318418+davidbadin@users.noreply.github.com>
2020-08-25 14:55:43 +0200

    calcul

--- PaladinsScoresheet/webapp/controller/Main.controller.js
+++ PaladinsScoresheet/webapp/controller/Main.controller.js
@@ -6,6 +6,17 @@ sap.ui.define([
 	return Controller.extend("Project.PaladinsScoresheet.controller.Main", {
 		onInit: function () {
 
+			var oView = this.getView();
+			var oDpdValues = oView.getModel("dropdownValues");
+			var aData = [];
+
+			for (var i = 0; i <= 12; i++) {
+				aData[i] = {"key": i}
+			}
+
+			oDpdValues.setProperty("/dpdAttr", aData);
+
+			// console.log(oDpdValues); // eslint-disable-line no-console
 		},
 		
 		onSelectedPlayersNumber: function () {
@@ -59,6 +70,9 @@ sap.ui.define([
 			
 			var oView = this.getView();
 			var iScore = 0;
+			var iResources = 0;
+			var sPlayerName;
+			var oI18N = oView.getModel("i18n").getResourceBundle();
 
 			// Completed king's orders
 			if ( oView.byId("vp4Box0" + iCol).getSelected() ) {
@@ -73,9 +87,9 @@ sap.ui.define([
 
 			iScore = iScore 
 			// Attribute tracks
-				+ parseInt(oView.byId("dpdStrength0" + iCol).getValue())
-				+ parseInt(oView.byId("dpdFaith0" + iCol).getValue())
-				+ parseInt(oView.byId("dpdInfluence0" + iCol).getValue())
+				+ this.convertAttrToVP ( oView.byId("dpdStrength0" + iCol).getValue() )
+				+ this.convertAttrToVP ( oView.byId("dpdFaith0" + iCol).getValue())
+				+ this.convertAttrToVP ( oView.byId("dpdInfluence0" + iCol).getValue())
 			// VP for actions
 				+ parseInt(oView.byId("dpdDevelop0" + iCol).getValue())
 				+ parseInt(oView.byId("dpdFortify0" + iCol).getValue())
@@ -85,30 +99,43 @@ sap.ui.define([
 			
 			// VP on walls
 			if ( oView.byId("inputFortifyExtra0" + iCol).getValue() ) {
-				iScore = iScore + parseInt(oView.byId("inputFortifyExtra0" + iCol).getValue())
+				iScore = iScore + parseInt(oView.byId("inputFortifyExtra0" + iCol).getValue());
 			}
 
 			// Converts
 			if ( oView.byId("inputConvert0" + iCol).getValue() ) {
-				iScore = iScore + parseInt(oView.byId("inputConvert0" + iCol).getValue())
+				iScore = iScore + parseInt(oView.byId("inputConvert0" + iCol).getValue());
 			}
 
 			// Unpaid debts
 			if ( oView.byId("inputDebtUnpaid0" + iCol).getValue() ) {
-				iScore = iScore + parseInt(oView.byId("inputDebtUnpaid0" + iCol).getValue())
+				iScore = iScore - ( 3 * parseInt(oView.byId("inputDebtUnpaid0" + iCol).getValue()) );
 			}
 
 			// Paid debts
 			if ( oView.byId("inputDebtPaid0" + iCol).getValue() ) {
-				iScore = iScore + parseInt(oView.byId("inputDebtPaid0" + iCol).getValue())
+				iScore = iScore + parseInt(oView.byId("inputDebtPaid0" + iCol).getValue());
 			}
 
-			// Silver and provision
+			// Silver
 			if ( oView.byId("inputSilver0" + iCol).getValue() ) {
-				iScore = iScore + parseInt(oView.byId("inputSilver0" + iCol).getValue())
+				iResources = iResources + parseInt(oView.byId("inputSilver0" + iCol).getValue());
+			}
+
+			// Provision
+			if ( oView.byId("inputProvision0" + iCol).getValue() ) {
+				iResources = iResources + parseInt(oView.byId("inputProvision0" + iCol).getValue());
 			}
 			
-			oView.byId("textScore0" + iCol).setText(iScore);
+			iScore = iScore + ( Math.floor( iResources / 3) );
+			
+			sPlayerName = oView.byId("inputPlayer0" + iCol).getValue();
+			if ( !sPlayerName ) {
+				sPlayerName = oI18N.getText("player") + " " + iCol;
+			}
+
+			oView.byId("textScore0" + iCol).setText(sPlayerName + ":" + "\n" + iScore + " " + oI18N.getText("points"));
+			
 		},
 
 		onReset: function () {
@@ -141,6 +168,51 @@ sap.ui.define([
 
 				oView.byId("textScore0" + iCol).setText("0");
 			}
+		},
+
+		convertAttrToVP: function (iAttr) {
+			var iVP = 0;
+
+			switch (iAttr) {
+				case "2":
+					iVP = 1;
+					break;
+				case "3":
+					iVP = 1;
+					break;
+				case "4":
+					iVP = 3;
+					break;
+				case "5":
+					iVP = 3;
+					break;
+				case "6":
+					iVP = 6;
+					break;
+				case "7":
+					iVP = 6;
+					break;
+				case "8":
+					iVP = 9;
+					break;
+				case "9":
+					iVP = 11;
+					break;
+				case "10":
+					iVP = 13;
+					break;
+				case "11":
+					iVP = 16;
+					break;
+				case "12":
+					iVP = 20;
+					break;
+				default:
+					iVP = 0;
+			}
+
+			return iVP;
+
 		}
 
 	});
