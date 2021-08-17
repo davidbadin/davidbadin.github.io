891f9e3b6709ae66fafbd9d551965e461e1d5021
davidbadin <61318418+davidbadin@users.noreply.github.com>
2021-08-17 22:13:35 +0200

    Update Main.controller.js

--- PD2021/webapp/controller/Main.controller.js
+++ PD2021/webapp/controller/Main.controller.js
@@ -305,39 +305,39 @@ sap.ui.define([
 			var iLength;
 			var iNumberOfRows = 0;	
 
-			oSourceInfoModel.loadData(sUri).then(function () {
+			// oSourceInfoModel.loadData(sUri).then(function () {
 
-				console.log( oSourceInfoModel.getData() );
-				if ( oSourceInfoModel.getProperty("/version") ) {
-					console.log( "Info Loaded");
+			console.log( oSourceInfoModel.getData() );
+			if ( oSourceInfoModel.getProperty("/version") ) {
+				console.log( "Info Loaded");
 
-					oInfoData = oSourceInfoModel.getData().feed.entry;
-					iLength = oSourceInfoModel.getProperty("/feed/entry/length");		
-					
-					for (var i = 0; i < iLength; i++) {
-						var iRow = oInfoData[i].gs$cell.row;				
-						var iCol = oInfoData[i].gs$cell.col;				
-						var sValue = oInfoData[i].gs$cell.$t;
+				oInfoData = oSourceInfoModel.getData().feed.entry;
+				iLength = oSourceInfoModel.getProperty("/feed/entry/length");		
+				
+				for (var i = 0; i < iLength; i++) {
+					var iRow = oInfoData[i].gs$cell.row;				
+					var iCol = oInfoData[i].gs$cell.col;				
+					var sValue = oInfoData[i].gs$cell.$t;
 
-						if (!aData[iRow - 1]) {
-							aData[iRow - 1] = [];
-						}
-						aData[iRow - 1][iCol - 1] = sValue;
-						iNumberOfRows = iRow;
+					if (!aData[iRow - 1]) {
+						aData[iRow - 1] = [];
 					}
+					aData[iRow - 1][iCol - 1] = sValue;
+					iNumberOfRows = iRow;
+				}
 
-					for (var j = 0; j < iNumberOfRows; j++) {
-						if ( aData[j] ) {
-							sInfo = sInfo + aData[j][0] + "\n" + "\n" + aData[j][1] + "\n" + "\n" + " ***** " + "\n" + "\n"
-						}
+				for (var j = 0; j < iNumberOfRows; j++) {
+					if ( aData[j] ) {
+						sInfo = sInfo + aData[j][0] + "\n" + "\n" + aData[j][1] + "\n" + "\n" + " ***** " + "\n" + "\n"
 					}
+				}
 
-					oInfoModel.setProperty("/info", sInfo);
+				oInfoModel.setProperty("/info", sInfo);
 
-				} else {
-					console.log( "Info NOT Loaded");
-				}		
-			});
+			} else {
+				console.log( "Info NOT Loaded");
+			}		
+			// });
 
 			oSourceInfoModel.detachRequestCompleted(this.afterInfoLoaded, this);
 
