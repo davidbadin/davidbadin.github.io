7fe227a81a5e547ff4dc0a4b254fdb9311ed7cb4
davidbadin <61318418+davidbadin@users.noreply.github.com>
2026-06-19 20:04:28 +0200

    upd

--- PD2026_app/app/build.gradle.kts
+++ PD2026_app/app/build.gradle.kts
@@ -30,8 +30,8 @@ android {
         applicationId = "sk.punkacidetom.pd2026"
         minSdk = 24
         targetSdk = 35
-        versionCode = 5
-        versionName = "1.0.0"
+        versionCode = 6
+        versionName = "1.0.6"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
